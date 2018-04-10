d84699415c3f57f0e87ffe2abfb847cc7779d2a6
bafraikin <fraikin.baptiste@gmail.com>
2018-04-10 15:19:16 +0200

    ex02

---
+++ ex02rot/rot_cesar_chiffre.rb
@@ -0,0 +1,17 @@
+
+class String
+  def chiffre_de_cesar(nb=5)
+    a = self.split("").collect do |char|
+      if char.ord >= 65 && char.ord <= 90
+        ((((char.ord - 'A'.ord) + nb) % 26) + 'A'.ord).chr
+      elsif char.ord >=97 && char.ord <=122
+        ((((char.ord - 'a'.ord + nb)) % 26) + 'a'.ord).chr
+      else
+        char
+      end
+    end
+    a.join("")
+  end 
+end
+
+puts "What a string!".chiffre_de_cesar
